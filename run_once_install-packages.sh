7352bc4debc84d293fa21306ffdcaed1ed8f01cd
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-04-20 11:17:45 -0600

    Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -7,18 +7,20 @@ sudo apt install build-essential pkg-config libssl-dev curl gh
 curl https://sh.rustup.rs -sSf | sh
 source "$HOME/.cargo/env"
 
-# Install Sheldon (https://sheldon.cli.rs/)
-if [ "$(uname)" == "Darwin" ]; then
-    brew install cmake
-    brew install sheldon
-    brew install pyenv
-else
-    cargo install sheldon
-    curl https://pyenv.run | bash
-fi
+# Install Homebrew (https://brew.sh/)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install cmake (https://cmake.org/)
+brew install cmake
+
+# Instal Sheldon (https://sheldon.cli.rs/)
+brew install sheldon
+
+# Install Pyenv (https://github.com/pyenv/pyenv)
+brew install pyenv
 
 # Install Starship (https://starship.rs/)
 cargo install starship --locked
 
-# Install NVM (https://neovim.io/)
+# Install NVM (https://github.com/nvm-sh/nvm)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
