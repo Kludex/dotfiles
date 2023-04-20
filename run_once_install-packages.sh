11b6160f23a963b364b19871ffa91b5958acf00b
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-04-20 11:04:13 -0600

    Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -3,9 +3,20 @@
 # Install Essentials
 sudo apt install build-essential pkg-config libssl-dev curl gh
 
-# Install Cargo
+# Install Cargo (https://doc.rust-lang.org/cargo/getting-started/installation.html)
 curl https://sh.rustup.rs -sSf | sh
+source "$HOME/.cargo/env"
 
-# Install NVM
+# Install Sheldon (https://sheldon.cli.rs/)
+if [ "$(uname)" == "Darwin" ]; then
+    brew install cmake
+    brew install sheldon
+else
+    cargo install sheldon
+fi
+
+# Install Starship (https://starship.rs/)
+cargo install starship --locked
+
+# Install NVM (https://neovim.io/)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-
