3295892a4f20c7ba38bd537b6e4b38e4de995e35
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-07-01 20:57:09 +0000

    Add Homebrew to PATH before installing packages

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -7,6 +7,15 @@ source "$HOME/.cargo/env"
 # Install Homebrew (https://brew.sh/)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Add Homebrew to PATH for the rest of this script (macOS and Linux locations)
+if [ -x /opt/homebrew/bin/brew ]; then
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -x /usr/local/bin/brew ]; then
+	eval "$(/usr/local/bin/brew shellenv)"
+elif [ -x /home/linuxbrew/.linuxbrew/bin/brew ]; then
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
 # Install cmake (https://cmake.org/)
 brew install cmake
 
